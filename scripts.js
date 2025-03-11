8b2ee57e51efaad3cb85442d7834b40970b48279
mugsmonteria <59841337+mugsmonteria@users.noreply.github.com>
2025-03-11 11:21:13 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,4 +3,6 @@ const navMenu = document.getElementById('nav-menu');
 
 menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('show');
+    // Cambia el contenido entre ☰ y X
+    menuToggle.textContent = navMenu.classList.contains('show') ? '✖' : '☰';
 });
